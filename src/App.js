deb086e51941053a23abf97d7fb97ab0877ccd45
karthik.palamalai <karthik.palamalai@weareadaptive.com>
2019-05-13 13:27:15 -0400

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import serviceProvider from './serviceProvider';
+import Main from './main';
+import { SET_INITIAL_STATE_RESP } from './actions';
 import './App.css';
 
+import rootReducer from './reducers/rootreducer';
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const initState = () => dispatch => {
+   serviceProvider.initStateService(
+     {}, 
+     data => {
+        dispatch( { type: SET_INITIAL_STATE_RESP, initialParameters: data });
+     }, 
+     err => {});
+};
+
 function App() {
+  store.dispatch(initState());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 }
 
